6a9edcae9999e2acc6e1f54264e5318ce3b6f042
Alexandre <alexandre.dejour1@gmail.com>
2023-07-20 18:57:17 +0200

    Add playRound() function

--- RockPaperCisor.js
+++ RockPaperCisor.js
@@ -4,6 +4,54 @@ function getComputerChoice() {
   return choices[randomIndex];
 }
 
-const computerchoice = getComputerChoice();
-console.log(computerchoice);
+var computerSelection = getComputerChoice();
+console.log(computerSelection);
 
+var playerInput = "ROCK";
+const playerSelection = playerInput.toLowerCase();
+
+function computerPlayRock(computerSelection, playerSelection) {
+
+    if (playerSelection == "Cisor") {
+      console.log("You loose, " + computerSelection + " beat " + playerSelection);
+    } else if (playerSelection == "Paper") {
+      console.log("You win, " + playerSelection + " beat " + computerSelection);
+    }
+}
+
+function computerPlayPaper(computerSelection, playerSelection) {
+
+    if (playerSelection == "Cisor") {
+      console.log("You win, " + playerSelection + " beat " + computerSelection);
+    } else if (playerSelection == "Rock") {
+      console.log("You loose, " + computerSelection + " beat " + playerSelection);
+    }
+}
+
+function computerPlayCisor(computerSelection, playerSelection) {
+
+    if (playerSelection == "Paper") {
+      console.log("You loose, " + computerSelection + " beat " + playerSelection);
+    } else if (playerSelection == "Rock") {
+      console.log("You win, " + playerSelection + " beat " + computerSelection);
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+  getComputerChoice();
+  console.log(computerSelection);
+
+      if (computerSelection == "Rock") {
+        computerPlayRock();
+      } else if (computerSelection == "Paper") {
+        computerPlayPaper();
+      } else if (computerSelection == "Cisor") {
+        computerPlayCisor();
+      } else if (computerSelection == playerSelection) {
+        console.log("Equality, play another round!");
+      }
+
+}
+
+  playRound(playerSelection, computerSelection);
